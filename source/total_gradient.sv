4947e351c048c8791c618123d9cb60c205ef1fd4
Nilay Desai <desai32@purdue.edu>
2016-11-29 16:04:41 -0500

    Update total_gradient.sv

--- source/total_gradient.sv
+++ source/total_gradient.sv
@@ -1,16 +1,16 @@
  module total_gradient (  
   input signed [10:0] gy,
   input signed [10:0] gx,
-  output reg [7:0] G
+  output reg [7:0] g
 );
 
 
-wire signed [10:0] abs_gx,abs_gy;	//it is used to find the absolute value of gx and gy 
+//wire signed [10:0] abs_gx,abs_gy;	//it is used to find the absolute value of gx and gy 
 wire [10:0] sum;			//the max value is 255*8. here no sign bit needed. 
 
 
 assign sum = (gx+gy);
-assign G = (|sum[10:8])?8'hff : sum[7:0];
+assign g = (|sum[10:8])?8'hff : sum[7:0];
 
 endmodule 
   
