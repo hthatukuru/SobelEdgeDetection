837850a519684a18460d154a9f1a8284b684351b
Nilay Desai <desai32@purdue.edu>
2016-11-29 16:07:03 -0500

    Update tb_total_gradient.sv

--- source/tb_total_gradient.sv
+++ source/tb_total_gradient.sv
@@ -32,10 +32,76 @@ module tb_total_gradient
 	tb_gy = 0;
 	tb_expected_g = 0;
 	#10
-  assert (tb_expected_g == tb_g)
+ 	assert (tb_expected_g == tb_g)
 		$info("Correct g value for test case %d!", testcase);
 	else
 		$error("Incorrect g value for test case %d!", testcase);
+	  	#10
+	testcase = 1;
+	tb_gx = 3;
+	tb_gy = 4;
+	tb_expected_g = 5;
+	#10
+	assert (tb_expected_g == tb_g)
+		$info("Correct g value for test case %d!", testcase);
+	else
+		$error("Incorrect g value for test case %d!", testcase);
+
+	#10
+	testcase = 2;
+	tb_gx = 12;
+	tb_gy = 13;
+	tb_expected_g = 17;
+	#10
+	assert (tb_expected_g == tb_g)
+		$info("Correct g value for test case %d!", testcase);
+	else
+		$error("Incorrect g value for test case %d!", testcase);
+
+	#10
+	testcase = 3;
+	tb_gx = 24;
+	tb_gy = 106;
+	tb_expected_g = 108;
+	#10
+	assert (tb_expected_g == tb_g)
+		$info("Correct g value for test case %d!", testcase);
+	else
+		$error("Incorrect g value for test case %d!", testcase);
+
+	#10
+	testcase = 4;
+	tb_gx = 200;
+	tb_gy = 95;
+	tb_expected_g = 221;
+	#10
+	assert (tb_expected_g == tb_g)
+		$info("Correct g value for test case %d!", testcase);
+	else
+		$error("Incorrect g value for test case %d!", testcase);
+
+	#10
+	testcase = 5;
+	tb_gx = 213;
+	tb_gy = 156;
+	tb_expected_g = 255;
+	#10
+	assert (tb_expected_g == tb_g)
+		$info("Correct g value for test case %d!", testcase);
+	else
+		$error("Incorrect g value for test case %d!", testcase);
+
+	#10
+	testcase = 6;
+	tb_gx = 255;
+	tb_gy = 255;
+	tb_expected_g = 255;
+	#10
+	assert (tb_expected_g == tb_g)
+		$info("Correct g value for test case %d!", testcase);
+	else
+		$error("Incorrect g value for test case %d!", testcase);
+
   
   end
   
