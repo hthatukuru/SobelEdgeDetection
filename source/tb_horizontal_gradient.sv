aec6ddbf560e0dc60d690e16c140f75ce1bcfdc4
Nilay Desai <desai32@purdue.edu>
2016-11-30 17:01:55 -0500

    Update tb_horizontal_gradient.sv

--- source/tb_horizontal_gradient.sv
+++ source/tb_horizontal_gradient.sv
@@ -1,4 +1,3 @@
-
 // $Id: $
 // File name:   tb_horizontal_gradient.sv
 // Created:     4/22/2016
@@ -64,13 +63,103 @@ module tb_horizontal_gradient ();
 		  $info("Correct gx value for test case %d!", testcase);
 	  else
 		  $error("Incorrect gx value for test case %d!", testcase);
-    
+
+
+	#10
+
+	//testcase 2
+	testcase = 2;
+	tb_windowBuffer[0] = 255;
+    	tb_windowBuffer[1] = 255;
+    	tb_windowBuffer[2] = 255;
+    	tb_windowBuffer[3] = 255;
+    	tb_windowBuffer[4] = 255;
+    	tb_windowBuffer[5] = 255;
+    	tb_windowBuffer[6] = 255;
+    	tb_windowBuffer[7] = 255;
+    	tb_windowBuffer[8] = 255;
+
+	tb_start_calculations = 1;
+	tb_expected_gx = 0;  //update this value here 
+
+    	#10
+ 	  assert (tb_expected_gx == tb_gx)
+		  $info("Correct gx value for test case %d!", testcase);
+	  else
+		  $error("Incorrect gx value for test case %d!", testcase);
+
+	#10
+
+	//testcase 3
+	testcase = 3;
+	tb_windowBuffer[0] = 255;
+    	tb_windowBuffer[1] = 255;
+    	tb_windowBuffer[2] = 0;
+    	tb_windowBuffer[3] = 155;
+    	tb_windowBuffer[4] = 255;
+    	tb_windowBuffer[5] = 205;
+    	tb_windowBuffer[6] = 255;
+    	tb_windowBuffer[7] = 255;
+    	tb_windowBuffer[8] = 5;
+
+	tb_start_calculations = 1;
+	tb_expected_gx = 405;  //update this value here 
+
+    	#10
+ 	  assert (tb_expected_gx == tb_gx)
+		  $info("Correct gx value for test case %d!", testcase);
+	  else
+		  $error("Incorrect gx value for test case %d!", testcase);
+
+	#10
+
+
+	//testcase 4
+	testcase = 4;
+	tb_windowBuffer[0] = 40;
+    	tb_windowBuffer[1] = 255;
+    	tb_windowBuffer[2] = 32;
+    	tb_windowBuffer[3] = 255;
+    	tb_windowBuffer[4] = 255;
+    	tb_windowBuffer[5] = 100;
+    	tb_windowBuffer[6] = 0;
+    	tb_windowBuffer[7] = 255;
+    	tb_windowBuffer[8] = 1;
+
+	tb_start_calculations = 1;
+	tb_expected_gx = 317;  //update this value here 
+
+    	#10
+ 	  assert (tb_expected_gx == tb_gx)
+		  $info("Correct gx value for test case %d!", testcase);
+	  else
+		  $error("Incorrect gx value for test case %d!", testcase);
+	#10
+
+	//testcase 5
+	testcase = 5;
+	tb_windowBuffer[0] = 255;
+    	tb_windowBuffer[1] = 255;
+    	tb_windowBuffer[2] = 255;
+    	tb_windowBuffer[3] = 255;
+    	tb_windowBuffer[4] = 255;
+    	tb_windowBuffer[5] = 255;
+    	tb_windowBuffer[6] = 255;
+    	tb_windowBuffer[7] = 255;
+    	tb_windowBuffer[8] = 255;
+
+	tb_start_calculations = 0;
+	tb_expected_gx = 317;  //update this value here 
+
+    	#10
+ 	  assert (tb_expected_gx == tb_gx)
+		  $info("Correct gx value for test case %d!", testcase);
+	  else
+		  $error("Incorrect gx value for test case %d!", testcase);
+
+	
   end
 endmodule
-  
-  
-  
-  
   
   
   
