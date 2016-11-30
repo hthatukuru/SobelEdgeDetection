6a07b50a674e98aaa0cf585756cff88c841be56e
Zhihao Lin <lin432@purdue.edu>
2016-11-30 17:58:37 -0500

    move control

---
+++ source/move_control.sv
@@ -0,0 +1,120 @@
+module move_control(
+
+//input
+	input wire clk, n_reset,
+	input reg [11:0] width,
+	input reg [11:0] length,
+	input reg [7:0] initial_addr_r,
+	input reg [7:0] initial_addr_w,
+	input reg start_move,
+	input reg load_initial,
+//output
+	output reg [7:0] addr_r,
+	output reg [7:0] addr_w,
+	output reg [1:0]direction, //0 for right, 1 for left, 2 for up
+	output reg all_done, 
+	output reg move_done
+	
+);
+   reg 		   movex, movey, x, y;
+   reg 		   next_addr_r, next_addr_w, next_x, next_y, next_all_done, next_movex, next_movey, next_move_done, next_direction;
+   
+
+   always_ff @ (negedge n_reset, posedge clk)
+     begin 
+	if (n_reset == 0)
+	  begin
+	     addr_r <= initial_addr_r;
+	     addr_w <= initial_addr_w;
+	     x <= 1;
+	     y <= 1;
+	     all_done <= 0;
+	     movex <= 0;
+	     movey <= 0;
+	     move_done <= 0;
+	     direction <= 2'b01;     
+	  end
+	else
+	  begin
+	     addr_r <= next_addr_r;
+	     addr_w <= next_addr_w;
+	     x <= next_x;
+	     y <= next_y;
+	     all_done <= next_all_done;
+	     movex <= next_movex;
+	     movey <= next_movey;
+	     move_done <= next_move_done;
+	     direction <= next_direction;
+	  end
+	end
+
+always_comb
+	begin
+	if (load_initial == 1)
+		begin
+		   next_addr_r = initial_addr_r;
+		   next_addr_w = initial_addr_w;
+		   next_x = 1;
+		   next_y = 1;
+		   next_all_done = 0;
+		   next_move_done = 0;
+		   next_direction = 2'b01;
+		end 
+	else if (start_move == 1)
+		begin
+		   next_movex = 0;
+		   next_movey = 0;
+		   next_move_done = 0;
+// direction
+		if (x == 1 && direction == 2'b10)
+			begin
+			   next_direction = 2'b11;
+			end
+		else if (x == (length - 2) && direction == 2'b01)
+			begin
+			   next_direction = 2'b11;
+			end
+		else if (y == width && direction == 2'b11)
+			begin
+			   next_all_done = 1;
+			end
+		else if (x == 1 && direction == 2'b11)
+			begin
+			   next_direction = 2'b01;
+			end
+		else if (x == (length - 2) && direction == 2'b11)
+			begin
+			   next_direction = 2'b10;
+			end
+
+// address
+		if (direction == 2'b01) //move(right, left, up)
+			begin
+			   next_addr_r = addr_r + 1;			   
+			   next_addr_w = addr_w + 1;
+			   next_movex = 1;
+			   next_x = x + 1;
+			end
+		else if (direction == 2'b10)
+			begin
+			   next_addr_r = addr_r - 1;
+			   next_addr_w = addr_w - 1;
+			   next_movex = 1;
+			   next_x = x - 1;
+			end
+		else if (direction == 2'b11)
+			begin
+			   next_addr_r = addr_r + length;
+			   next_addr_w = addr_w + length;
+			   next_movey = 1;
+			   next_y = y + 1;
+			end
+
+//determine move is finished
+		if (movex == 1 || movey == 1) 
+			begin
+			   next_move_done = 1;			   
+			end
+		end
+	end
+endmodule
---
+++ source/tb_move_control.sv
@@ -0,0 +1,143 @@
+`timescale 1ns / 100ps
+
+module tb_move_control
+();
+   localparam   CLK_PERIOD = 2.5;
+   localparam	CHECK_DELAY = 1; 
+   reg tb_clk;
+   reg tb_n_reset;
+   
+	reg [11:0] tb_width;
+	reg [11:0] tb_length;
+	reg [7:0] tb_initial_addr_r;
+	reg [7:0] tb_initial_addr_w;
+	reg tb_start_move;
+	reg tb_load_initial;
+	reg [7:0]tb_addr_r;
+	reg [7:0]tb_addr_w;
+	reg [1:0]tb_direction;
+	reg tb_all_done;
+	reg tb_move_done;	
+  
+  integer testcase;
+	reg [7:0] tb_expected_addr_r;
+	reg [7:0] tb_expected_addr_w;
+	reg [1:0] tb_expected_direction;
+	reg tb_expected_all_done;
+	reg tb_expected_move_done;
+  
+  
+  move_control DUT (.clk(tb_clk), .n_reset(tb_n_reset), .width(tb_width), .length(tb_length), .initial_addr_r(tb_initial_addr_r), .initial_addr_w(tb_initial_addr_w), .start_move(tb_start_move), .load_initial(tb_load_initial), .addr_r(tb_addr_r), .addr_w(tb_addr_w), .direction(tb_direction), .all_done(tb_all_done), .move_done(tb_move_done));
+   
+  	always
+	begin
+		tb_clk = 1'b0;
+		#(CLK_PERIOD/2.0);
+		tb_clk = 1'b1;
+		#(CLK_PERIOD/2.0);
+	end
+  
+  initial
+    begin
+  
+  //initialization
+       testcase = 0;	
+       tb_width = 5;
+       tb_length = 5;
+       tb_initial_addr_r = 100;
+       tb_initial_addr_w = 0;
+       tb_start_move = 0;
+       tb_load_initial = 1;
+       tb_expected_addr_r = 100;
+       tb_expected_addr_w = 0;
+       tb_expected_direction = 2'b01;
+       tb_expected_all_done = 0;
+       tb_expected_move_done = 0;
+       #10
+         assert (tb_expected_addr_r == tb_addr_r)
+	   $info("Correct initial read address for test case %d!", testcase);
+	else
+	  $error("Incorrect initial read address for test case %d!", testcase);
+       testcase = 1;
+         assert (tb_expected_addr_w == tb_addr_w)
+	   $info("Correct initial write address for test case %d!", testcase);
+	else
+	  $error("Incorrect initial write address for test case %d!", testcase);
+       testcase = 2;
+       assert (tb_expected_direction == tb_direction)
+	 $info("Correct initial direction for test case %d!", testcase);
+        else
+	  $error("Incorrect initial direction for test case %d!", testcase);
+       
+       #10
+       testcase = 3;
+       tb_load_initial = 0;
+       tb_start_move = 1;
+       tb_expected_addr_r = 101;
+       tb_expected_addr_w = 1;
+       tb_expected_direction = 2'b01;       
+       #10
+         assert (tb_expected_addr_r == tb_addr_r)
+	   $info("Correct read address for test case %d!", testcase);
+	else
+	  $error("Incorrect read address for test case %d!", testcase);
+       testcase = 4;
+         assert (tb_expected_addr_w == tb_addr_w)
+	   $info("Correct write address for test case %d!", testcase);
+	else
+	  $error("Incorrect write address for test case %d!", testcase); 
+       testcase = 5;
+       assert (tb_expected_direction == tb_direction)
+	 $info("Correct initial direction for test case %d!", testcase);
+        else
+	  $error("Incorrect initial direction for test case %d!", testcase);
+       tb_start_move = 0;
+       #10
+       testcase = 6;
+       tb_start_move = 1;
+       tb_expected_addr_r = 102;
+       tb_expected_addr_w = 2;
+       tb_expected_direction = 2'b01;       
+       #10
+         assert (tb_expected_addr_r == tb_addr_r)
+	   $info("Correct read address for test case %d!", testcase);
+	else
+	  $error("Incorrect read address for test case %d!", testcase);
+       testcase = 7;
+         assert (tb_expected_addr_w == tb_addr_w)
+	   $info("Correct write address for test case %d!", testcase);
+	else
+	  $error("Incorrect write address for test case %d!", testcase); 
+       testcase = 8;
+       assert (tb_expected_direction == tb_direction)
+	 $info("Correct change direction to up for test case %d!", testcase);
+        else
+	  $error("Incorrect change direction to up for test case %d!", testcase);    
+       tb_start_move = 0;
+       #10
+       testcase = 9;
+       tb_start_move = 1;
+       tb_expected_addr_r = 107;
+       tb_expected_addr_w = 7;
+       tb_expected_direction = 2'b11;       
+       #10
+         assert (tb_expected_addr_r == tb_addr_r)
+	   $info("Correct read address for test case %d!", testcase);
+	else
+	  $error("Incorrect read address for test case %d!", testcase);
+       testcase = 10;
+         assert (tb_expected_addr_w == tb_addr_w)
+	   $info("Correct write address for test case %d!", testcase);
+	else
+	  $error("Incorrect write address for test case %d!", testcase); 
+       testcase = 11;
+       assert (tb_expected_direction == tb_direction)
+	 $info("Correct change direction to up for test case %d!", testcase);
+        else
+	  $error("Incorrect change direction to up for test case %d!", testcase);         
+	  
+
+    end // initial begin
+   
+endmodule
+
