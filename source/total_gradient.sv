c85febb3e29daabfa900bd019226ad38a653fabb
Nilay Desai <desai32@purdue.edu>
2016-11-30 16:22:45 -0500

    Update total_gradient.sv

--- source/total_gradient.sv
+++ source/total_gradient.sv
@@ -1,7 +1,7 @@
  module total_gradient (  
   input signed [10:0] gy,
   input signed [10:0] gx,
-  output reg [7:0] g
+  output wire [7:0] g
 );
 
 
@@ -9,10 +9,12 @@
 wire [10:0] sum;			//the max value is 255*8. here no sign bit needed. 
 
 
-assign sum = (gx+gy);
-//assign g = (|sum[10:8])?8'hff : sum[7:0];
-  g = (sum[10] | sum[9] | sum[8]) ? 8’b11111111 : sum[7:0]
+	assign sum = (gx+gy);
+//assign g =(|sum[10:8])?8'hff : sum[7:0];
+  
 
+	
+	assign g = (sum[10] | sum[9] | sum[8]) ? 8'b11111111 : sum[7:0];
 
-endmodule 
+endmodule
   
