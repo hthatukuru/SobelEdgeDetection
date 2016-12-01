f0ef2fa007825f68d8321358de3f26b38cddc617
Harika Thatukuru <hthatukuru@users.noreply.github.com>
2016-11-30 21:14:28 -0500

    Update windowBuffer.sv

--- source/windowBuffer.sv
+++ source/windowBuffer.sv
@@ -5,25 +5,39 @@
 
 module windowBuffer
 (
+  input n_rst,
+  input clk,
   input reg start_shift,
   input reg start_read,
   input reg [1:0] shift_direc,
   input reg [7:0] data_r,
-  input reg [7:0] windowBufferIn[0:8],
+  // input reg [7:0] windowBufferIn[0:8],
   output reg read_done,
   output reg shift_done,
   output reg [7:0] windowBuffer[0:8]
 );
 
   reg [7:0] tempWindowBuffer[0:8];
-  reg i = 0;
-  reg notFound = 1;
-  reg firstEmpty = 0;
+  reg count;
+  reg nextCount;
   
+  always_ff @ (negedge n_rst, posedge clk)
+	begin :	resetting
+		if( n_rst == 1'b0 )
+		begin
+			count <= 0;
+		end
+		else
+		begin
+			count <= nextCount;
+		end
+	end
+
   always_comb
   begin
-    tempWindowBuffer = windowBufferIn;
-    
+    // tempWindowBuffer = windowBufferIn;
+    nextCount = count;
+
     if(start_shift == 1)
     begin
 
@@ -54,8 +68,6 @@ module windowBuffer
         tempWindowBuffer [6] = 0;
         tempWindowBuffer [3] = 0;
         tempWindowBuffer [0] = 0;
-
-        
         
         read_done = 0;
         shift_done = 1;
@@ -77,25 +89,107 @@ module windowBuffer
         shift_done = 1;
       end
       
+	nextCount = 0;
     end
     
     else if(start_read == 1)
     begin
       
-      while ( (i < 9) && notFound )
-      begin
-        if (tempWindowBuffer[i] == 0)
-          begin
-            firstEmpty = i;
-            notFound = 0;
-          end
-        i = i + 1;
-      end
-      
-      tempWindowBuffer[firstEmpty] = data_r;
-      read_done = 1;
-      shift_done = 0;
-      
+	if(shift_direc == 2'b00)
+	begin
+		if(nextCount == 0)
+		begin
+			tempWindowBuffer[6] = data_r;
+		end
+		else if(nextCount == 1)
+		begin
+			tempWindowBuffer[7] = data_r;
+		end
+		else if(nextCount == 2)
+		begin
+			tempWindowBuffer[8] = data_r;
+		end
+		else if(nextCount == 3)
+		begin
+			tempWindowBuffer[3] = data_r;
+		end
+		else if(nextCount == 4)
+		begin
+			tempWindowBuffer[4] = data_r;
+		end
+		else if(nextCount == 5)
+		begin
+			tempWindowBuffer[5] = data_r;
+		end
+		else if(nextCount == 6)
+		begin
+			tempWindowBuffer[0] = data_r;
+		end
+		else if(nextCount == 7)
+		begin
+			tempWindowBuffer[1] = data_r;
+		end
+		else if(nextCount == 8)
+		begin
+			tempWindowBuffer[2] = data_r;
+		end
+		
+		nextCount++;
+	end
+	else if(shift_direc == 2'b01)
+	begin
+		if(nextCount == 0)
+		begin
+			tempWindowBuffer[8] = data_r;
+		end
+		else if(nextCount == 1)
+		begin
+			tempWindowBuffer[5] = data_r;
+		end
+		else if(nextCount == 2)
+		begin
+			tempWindowBuffer[2] = data_r;
+		end
+
+		nextCount = nextCount + 1;
+	end
+	else if(shift_direc == 2'b10)
+	begin
+		if(nextCount == 0)
+		begin
+			tempWindowBuffer[6] = data_r;
+		end
+		else if(nextCount == 1)
+		begin
+			tempWindowBuffer[3] = data_r;
+		end
+		else if(nextCount == 2)
+		begin
+			tempWindowBuffer[0] = data_r;
+		end
+
+		nextCount = nextCount + 1;
+	end
+        else if(shift_direc == 2'b11)
+	begin
+		if(nextCount == 0)
+		begin
+			tempWindowBuffer[0] = data_r;
+		end
+		else if(nextCount == 1)
+		begin
+			tempWindowBuffer[1] = data_r;
+		end
+		else if(nextCount == 2)
+		begin
+			tempWindowBuffer[2] = data_r;
+		end
+
+		nextCount = nextCount + 1;
+	end
+
+	read_done = 1;
+        shift_done = 0;
     end
     
     windowBuffer = tempWindowBuffer;
