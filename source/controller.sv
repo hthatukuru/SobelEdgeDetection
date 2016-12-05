cf761a9b8ae22c18149571e8ca3ddd2f3d24e567
Nilay Desai <desai32@purdue.edu>
2016-12-05 16:24:54 -0500

    Update controller.sv

--- source/controller.sv
+++ source/controller.sv
@@ -6,7 +6,7 @@ ouput reg start_write, start_move, start_shift, start_read, start_calculation
 );
 
 
-    typedef enum bit [7:0] {IDLE, LOAD_PARAM, L_PIXEL1, L_PIXEL2, L_PIXEL3, L_PIXEL4, L_PIXEL5, L_PIXEL6, L_PIXEL7, L_PIXEL8, L_PIXEL9, GRADIENT, T_GRADIENT, CHECK_EDGE, WRITE, CHECK_PIXEL_LR, CHECK_PIXEL_U, SHIFT_LR, READ_P1, READ_P2, READ_P2, READ_P3, SHIFT_D, READ_UP1, READ_UP2, READ_UP3, IMAGE_DONE} stateType;
+    typedef enum bit [7:0] {IDLE, LOAD_PARAM, L_PIXEL1, L_PIXEL2, L_PIXEL3, L_PIXEL4, L_PIXEL5, L_PIXEL6, L_PIXEL7, L_PIXEL8, L_PIXEL9, GRADIENT, T_GRADIENT, CHECK_EDGE, WRITE, READ_P1, READ_P2, READ_P3, IMAGE_DONE} stateType;
 stateType state;
 stateType next_state;
 
@@ -15,6 +15,11 @@ begin : REG_LOGIC
     if (n_rst == 0)
     begin
         state<=IDLE;
+        start_write <= 0;
+        start_move <= 0;
+        start_shift <= 0;
+        start_read <= 0;
+        start_calculation <= 0;
     end
     else
     begin
