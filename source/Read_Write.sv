25b725384d68f9d6e6b5d98242751fd150c695ff
Harika Thatukuru <hthatukuru@users.noreply.github.com>
2016-12-07 01:11:43 -0500

    Update Read_Write.sv

--- source/Read_Write.sv
+++ source/Read_Write.sv
@@ -12,12 +12,14 @@ output reg [1:0] instruction, //00 busy 01 read 10 wirte
 output reg [7:0] addr_w,
 output reg [7:0] addr_r,
 output reg write_done,
+output reg read_data_done,
 output reg [7:0] data_r_o
 );
 
 always_comb
   begin
      write_done = 0;
+     read_data_done = 0;
      //instruction = 2'b00;
    if (busy)
      begin
@@ -30,7 +32,7 @@ always_comb
 	     instruction = 2'b01;
 	     addr_r = addr_r_mc;
 	     data_r_o = data_r;
- 
+	     read_data_done = 1;
 	  end
 
 
