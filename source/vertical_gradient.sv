8e86d5e1a1327b70cc877ef5610e5a8c0f344234
Nilay Desai <desai32@purdue.edu>
2016-12-14 20:41:14 -0500

    Update vertical_gradient.sv

--- source/vertical_gradient.sv
+++ source/vertical_gradient.sv
@@ -1,39 +1,44 @@
-module vertical_gradient(
-	
-	input reg [7:0] windowBuffer[0:8],
-	input reg start_calculations,
-	output signed [10:0] gy
-	
+  module total_gradient (  
+  input signed [10:0] gy,
+  input signed [10:0] gx,
+  input reg start_t_grad,
+  output reg [7:0] g
 );
 
-	reg signed [10:0] abs_gy;
-	wire [7:0] P0;
-	wire [7:0] P1;
-	wire [7:0] P2;
-	wire [7:0] P3;
-	wire [7:0] P4;
-	wire [7:0] P5;
-	wire [7:0] P6;
-	wire [7:0] P7;
-	wire [7:0] P8;
+
+//wire signed [10:0] abs_gx,abs_gy;	//it is used to find the absolute value of gx and gy 
+reg [10:0] sum;			//the max value is 255*8. here no sign bit needed. 
+reg [10:0] final_sum;
+
+	//assign sum = (gx+gy);
+//assign g =(|sum[10:8])?8'hff : sum[7:0];
 	
+  
+
 	
-	assign P0 = windowBuffer[0];
-	assign P1 = windowBuffer[1];
-	assign P2 = windowBuffer[2];
-	assign P3 = windowBuffer[3];
-	assign P4 = windowBuffer[4];
-	assign P5 = windowBuffer[5];
-	assign P6 = windowBuffer[6];
-	assign P7 = windowBuffer[7];
-	assign P8 = windowBuffer[8];
+		//assign g = (start_t_grad ? ((sum[10] | sum[9] | sum[8]) ? 8'b11111111 : sum[7:0]) : 0;
+	//assign final_sum = (sum[10] | sum[9] | sum[8]) ? 8'b11111111 : sum[7:0];
 	
+	//assign g = (sum[10] | sum[9] | sum[8]) ? 8'b11111111 : sum[7:0];
+
 	always_comb
 	begin
-		if (start_calculations == 1)
-			abs_gy = ((P0-P6)+((P1-P7)<<1)+(P2-P8)); //sobel mask for gradient in vertical direction 
+		if (start_t_grad == 1)
+		begin
+			sum = (gx+gy);
+			if (sum[10] | sum[9] | sum[8])
+				g = 8'b11111111;
+			else
+				g = sum[7:0];
+		end
+		else
+		begin
+			g = 0;
+		end
 	end
-	
-	assign gy = (abs_gy[10]? ~abs_gy+1 : abs_gy);
 
+
+	
+	
 endmodule
+  
